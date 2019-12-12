c5fbd51408647ae52c462ae82530f3cba699d863
Dmitry <44303422+BupycSoftware@users.noreply.github.com>
2019-12-13 01:18:55 +0200

    sosu

--- МІй щоденник/src/app.js
+++ МІй щоденник/src/app.js
@@ -31,9 +31,11 @@ inputCheck();
 
 function updateDOM() {
     if (local_data.length) {
-        local_data.forEach(obj => {
-            newNoteCreator();
-        });
+        newNoteCreator();
+    } else {
+        while (rootNode.lastChild) {
+            rootNode.removeChild(rootNode.lastChild);
+        }
     }
 }
 mainInput.addEventListener('input', inputCheck);
@@ -63,131 +65,143 @@ function addData(event) {
         event.preventDefault();                                 // вимикаємо кнопку, якщо відсутні тема та запис
     } else {
         let obj = {};
+
         obj.value = mainInput.value;
         obj.themeInput = themeInput.value;
 
-        newNoteCreator()
+        let d = new Date();
+        let day = d.getDate();
+        let month = d.getMonth() + 1;
+        let year = d.getFullYear();
+        obj.time = d.toLocaleTimeString();
+        obj.date = day + '.' + month + '.' + year;
+
+        local_data.push(obj);
+        localStorage.setItem('data', JSON.stringify(local_data));
+
+        updateDOM();
     }
 }
 
 function newNoteCreator() {
-           let obj = {};
-           let itemContainer = document.createElement('div'); // створення контейнера для нового запису
-           itemContainer.setAttribute('draggable', 'true');
-
-           let newItem = document.createElement('input');
-           newItem.setAttribute('type', 'text');
-           newItem.value = mainInput.value;
-           obj.value = mainInput.value;
-           newItem.id = 'itemId';
-           newItem.style.display = 'none';
-
-           let theme = document.createElement('div'); // додавання теми для створеного поля
-           theme.textContent = themeInput.value;
-           obj.theme = themeInput.value;
-
-           let dateContainer = document.createElement('div'); // контейнер для дати
-           dateContainer.className = 'dateContainer';                                                                                                                                                          
-           let currentDate = document.createElement('p'); 
-           let currentTime = document.createElement('p');
-           currentDate.className = 'currentDate';
-           let d = new Date();
-           let day = d.getDate();
-           let month = d.getMonth() + 1;
-           let year = d.getFullYear();
-           currentDate.textContent = day + '.' + month + '.' + year;
-           obj.time = d.toLocaleTimeString();
-           obj.date = day + '.' + month + '.' + year;
-           currentTime.textContent =  d.toLocaleTimeString();
-
-
-
-           local_data.push(obj);
-           localStorage.setItem('data', JSON.stringify(local_data));
-
-
-
-
-           let checkBox = document.createElement('i');
-           checkBox.className = 'material-icons added_ico_checkbox';
-           checkBox.textContent = 'check_box_outline_blank';
-           checkBox.addEventListener('click', unclick)
-
-           checkBox.addEventListener('click', () => {
-               checkBox.textContent = 'check_box';
-           });
-
-
-           let labelForItem = document.createElement('label');
-           labelForItem.textContent = newItem.value;
-           labelForItem.className = 'itemsLabel';
-           labelForItem.setAttribute('for', newItem.id);
-
-           let editIcon = document.createElement('i');
-           editIcon.className = 'material-icons';
-           editIcon.textContent = 'create';
-           editIcon.addEventListener('click', () => {
-               itemContainer.removeAttribute('draggable');
-           
-               checkBox.style.display = 'none';
-               newItem.style.display = 'inline-block';
-               labelForItem.style.display = 'none';
-               editIcon.style.display = 'none';
-               removeIcon.style.display = 'none';
-               saveChangesIcon.style.display = 'inline-block';
-           });
-
-           let removeIcon = document.createElement('i');
-           removeIcon.className = 'material-icons removeIcn';
-           removeIcon.textContent = 'delete';
-           removeIcon.addEventListener('click', removeItem)
-
-           let saveChangesIcon = document.createElement('i');
-           saveChangesIcon.className = 'material-icons';
-           saveChangesIcon.textContent = 'save';
-           saveChangesIcon.style.display = 'none';
-
-           saveChangesIcon.addEventListener('click', () => {         
-               labelForItem.textContent = newItem.value;
-           
-               checkBox.style.display = 'inline-block';
-               newItem.style.display = 'none';
-               labelForItem.style.display = 'inline-block';
-               editIcon.style.display = 'inline-block';
-               removeIcon.style.display = 'inline-block';
-               saveChangesIcon.style.display = 'none';
-           
-               itemContainer.setAttribute('draggable', 'true');                
-           });
-
-           itemContainer.appendChild(dateContainer);
-           dateContainer.appendChild(theme);
-           dateContainer.appendChild(currentDate);
-           dateContainer.appendChild(currentTime);
-           itemContainer.appendChild(newItem);              // додаємо елементи в розмітку
-           itemContainer.appendChild(labelForItem);
-           itemContainer.appendChild(editIcon);
-           itemContainer.appendChild(saveChangesIcon);
-           itemContainer.appendChild(removeIcon);
-           rootNode.appendChild(itemContainer);
-
-           mainInput.value = '';
-           themeInput.value = '';
-           addIcon.style.color = '#c8d0d8';
+    while (rootNode.lastChild) {
+        rootNode.removeChild(rootNode.lastChild);
     }
+    if (local_data.length !== 0) {
+        local_data.forEach(el => {
+            let itemContainer = document.createElement('div'); // створення контейнера для нового запису
+            itemContainer.setAttribute('draggable', 'true');
+
+            let newItem = document.createElement('input');
+            newItem.setAttribute('type', 'text');
+            newItem.value = el.value;
+            newItem.id = 'itemId';
+            newItem.style.display = 'none';
+
+            let theme = document.createElement('div'); // додавання теми для створеного поля
+            theme.textContent = el.themeInput;
+
+            let dateContainer = document.createElement('div'); // контейнер для дати
+            dateContainer.className = 'dateContainer';                                                                                                                                                          
+            let currentDate = document.createElement('p'); 
+            let currentTime = document.createElement('p');
+            currentDate.className = 'currentDate';
+            currentDate.textContent = el.date;
+            currentTime.textContent =  el.time;
+
+            let checkBox = document.createElement('i');
+            checkBox.className = 'material-icons added_ico_checkbox';
+            checkBox.textContent = 'check_box_outline_blank';
+            checkBox.addEventListener('click', unclick)
+
+            checkBox.addEventListener('click', () => {
+                checkBox.textContent = 'check_box';
+            });
+
+
+            let labelForItem = document.createElement('label');
+            labelForItem.textContent = newItem.value;
+            labelForItem.className = 'itemsLabel';
+            labelForItem.setAttribute('for', newItem.id);
+
+            let editIcon = document.createElement('i');
+            editIcon.className = 'material-icons';
+            editIcon.textContent = 'create';
+            editIcon.addEventListener('click', () => {
+                itemContainer.removeAttribute('draggable');
+                
+                checkBox.style.display = 'none';
+                newItem.style.display = 'inline-block';
+                labelForItem.style.display = 'none';
+                editIcon.style.display = 'none';
+                removeIcon.style.display = 'none';
+                saveChangesIcon.style.display = 'inline-block';
+            });
+
+            let removeIcon = document.createElement('i');
+            removeIcon.className = 'material-icons removeIcn';
+            removeIcon.textContent = 'delete';
+            removeIcon.addEventListener('click', removeItem)
+
+            let saveChangesIcon = document.createElement('i');
+            saveChangesIcon.className = 'material-icons';
+            saveChangesIcon.textContent = 'save';
+            saveChangesIcon.style.display = 'none';
+
+            saveChangesIcon.addEventListener('click', (e) => {     
+                let text = e.target.parentNode.children[2].textContent;
+                for (let i = 0; i < local_data.length; i++) {
+                    if (local_data[i].value === text) {
+                        local_data[i].value = newItem.value;
+                    }
+                }
+                localStorage.setItem('data', JSON.stringify(local_data));
+                updateDOM();   
+
+                labelForItem.textContent = newItem.value;
+            
+                checkBox.style.display = 'inline-block';
+                newItem.style.display = 'none';
+                labelForItem.style.display = 'inline-block';
+                editIcon.style.display = 'inline-block';
+                removeIcon.style.display = 'inline-block';
+                saveChangesIcon.style.display = 'none';
+            
+                itemContainer.setAttribute('draggable', 'true');                
+            });
+
+            itemContainer.appendChild(dateContainer);
+            dateContainer.appendChild(theme);
+            dateContainer.appendChild(currentDate);
+            dateContainer.appendChild(currentTime);
+            itemContainer.appendChild(newItem);              // додаємо елементи в розмітку
+            itemContainer.appendChild(labelForItem);
+            itemContainer.appendChild(editIcon);
+            itemContainer.appendChild(saveChangesIcon);
+            itemContainer.appendChild(removeIcon);
+            rootNode.appendChild(itemContainer);
+        });
+        mainInput.value = '';
+        themeInput.value = '';
+        addIcon.style.color = '#c8d0d8';
+    }
+}
+
 function unclick () {
     this.disabled = true;
 }
 
-function removeItem () {
-    let parrent = this.parentNode;
-    rootNode.removeChild(parrent);
-    addIcon.disabled = false;
-    mainInput.disabled = false;
-    errorNotification.style.display = 'none';
-    addIcon.style.color = '#41b5fe';
-    mainInput.value = '';
-    addIcon.style.color = '#c8d0d8';
+function removeItem(e) {
+    let text = e.target.parentNode.children[2].textContent;
+    for (let i = 0; i < local_data.length; i++) {
+        if (local_data[i].value === text) {
+            local_data.splice(i, 1);
+            i--;
+        }
+    }
+    localStorage.setItem('data', JSON.stringify(local_data));
+
+    updateDOM();
 }
 
 
